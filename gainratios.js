11939046cb62228a158f99badf049304c299f34a
Iain MacDonald <IJMacD@gmail.com>
2012-11-20 00:19:19 +0000

    Added gearhub handling code

--- gainratios.js
+++ gainratios.js
@@ -11,10 +11,15 @@ Array.prototype.remove = function(from, to) {
           //'test': [ [53], [19], 340, 170, "Test" ],
           'tdf': [ [52,39], [12,13,15,17,19,21,23,26], 336, 170, "Carrera Tdf", "rgb(255,255,0)", "rgb(0,0,0)" ],
           'triban3': [ [50,39,30], [12,13,15,17,19,21,23,25], 336, 170, "B'twin Triban 3", "rgb(255,0,0)", "rgb(96,0,0)" ],
-          'I1': [ [38], [16], 311, 170, "I1", "rgb(192,192,255)", "rgb(192,192,192)" ],
+          'I1': [ [38], [16], 312, 170, "I1", "rgb(192,192,255)", "rgb(192,192,192)" ],
           'I2': [ [52,42], [12,13,15,17,19,21,23,26], 336, 170, "I2", "rgb(255,255,0)", "rgb(0,0,0)" ],
           'supersix': [ [53,39], [11,12,13,14,15,16,17,19,21,23,25], 336, 170, "Cannondale Supersix Evo", "rgb(128,128,128)", "rgb(0,0,0)" ],
           'nukeproof': [ [36], [11,12,13,14,15,17,19,21,23,26], 330, 170, "Nukeproof Snap Comp", "rgb(202,202,202)", "rgb(40,40,40)" ]
+        },
+        builtin_hubs = {
+          'nexus7': [0.632,0.741,0.843,0.989,1.145,1.335,1.545],
+          'alfine11': [0.527,0.681,0.77,0.878,0.995,1.134,1.292,1.462,1.667,1.888,2.153],
+          'rohloff14': [0.279,0.316,0.36,0.409,0.464,0.528,0.6,0.682,0.774,0.881,1,1.135,1.292,1.467]
         },
         i = 0,
         l = props.length,
@@ -25,6 +30,7 @@ Array.prototype.remove = function(from, to) {
         bikes = [],
         canvas = $('canvas')[0],
         ctx = canvas.getContext('2d');
+    builtin_bikes['I1'].gearhub = [0.632,0.741,0.843,0.989,1.145,1.335,1.545];
     for(;i<l;i++){
       (function(){
         var name = props[i].toLowerCase(),
@@ -39,6 +45,20 @@ Array.prototype.remove = function(from, to) {
         });
       })();
     }
+    $('a[href=#gearhub]').click(function(){
+      var sprocket_lst = $('#sprocket_lst'),
+          sprocket_txt = $('#sprocket_txt'),
+          gearhub_lst = $('#gearhub_lst')
+          val = sprocket_txt.val() || sprocket_lst.val();
+      sprocket_lst.hide();
+      $(this).hide();
+      $('a[href=#customSprocket]').hide();
+      selectSpecify();
+      val = val.split(" ")[0];
+      sprocket_txt.val(val);
+      sprocket_txt.css({width:"30px"}).show();
+      gearhub_lst.css({width:"170px"}).show();
+    });
     // Build Built in list
     for(b in builtin_bikes){
       $('<option>').attr("value",b).text(builtin_bikes[b][4]).appendTo(builtin_lst);
@@ -62,17 +82,15 @@ Array.prototype.remove = function(from, to) {
           max = 0,
           xscale,
           last_line,
-          x, y;
+          x, y,
+          title;
       canvas.height = l * 50 + 50;
       tooltips.empty();
       tooltips[0].style.top = canvas.offsetTop+"px";
       tooltips[0].style.left = canvas.offsetLeft+"px";
       for(i=0;i<l;i++){
         ratios = calculateGainRatios(bikes[i]);
-        m = ratios.length;
-        for(j=0;j<m;j++){
-          max = Math.max(max, ratios[j][0]);
-        }
+        max = Math.max(max, ratios[0][0]);
       }
       xscale = (canvas.width - 20) / max;
       last_line = Math.floor(max);
@@ -125,7 +143,11 @@ Array.prototype.remove = function(from, to) {
               ctx.strokeStyle = bikes[i][6];
               ctx.stroke();
             }
-            $('<a>').attr("title", bikes[i][0][j]+"x"+bikes[i][1][k]).css({left:(x-5)+"px",top:(y-5)+"px"}).tooltip().appendTo(tooltips);
+            if(typeof bikes[i].gearhub == "undefined")
+              title = bikes[i][0][j]+"×"+bikes[i][1][k];
+            else
+              title = bikes[i][0][j]+"×"+bikes[i][1][0]+"("+(k+1)+")";
+            $('<a>').attr("title", title).css({left:(x-5)+"px",top:(y-5)+"px"}).tooltip().appendTo(tooltips);
           }
         }
       }
@@ -136,19 +158,37 @@ Array.prototype.remove = function(from, to) {
           i = 0,
           j = 0,
           l = bike[0].length,
-          m = bike[1].length,
+          m,
           cl = bike[3],
           wr = bike[2],
           cr,
-          sp;
-      for(;i<l;i++){
-        o = []
-        for(j=0;j<m;j++){
+          sp,
+          r = wr / cl,
+          f;
+      if(bike.gearhub){
+        for(;i<l;i++){
+          o = [];
+          m = bike.gearhub.length;
           cr = bike[0][i];
-          sp = bike[1][j];
-          o.push((wr / cl) * (cr / sp));
+          sp = bike[1][0];
+          for(j=m-1;j>=0;j--){
+            f = bike.gearhub[j];
+            o.push(r * (cr / sp) * f);
+          }
+          out.push(o);
         }
-        out.push(o);
+      }
+      else{
+        for(;i<l;i++){
+          o = [];
+          m = bike[1].length;
+          cr = bike[0][i];
+          for(j=0;j<m;j++){
+            sp = bike[1][j];
+            o.push(r * (cr / sp));
+          }
+          out.push(o);
+        }
       }
       return out;
     }
@@ -167,9 +207,11 @@ Array.prototype.remove = function(from, to) {
             sprockets = $('#sprocket_txt').val(),
             wheelsize = $('#wheelsize_txt').val(),
             cranksize = $('#cranksize_txt').val(),
+            gearhub = $('#gearhub_lst'),
             name = $('#name_txt').val(),
             fore = "rgb("+rand()+","+rand()+","+rand()+")",
-            back = "rgb("+rand()+","+rand()+","+rand()+")";
+            back = "rgb("+rand()+","+rand()+","+rand()+")",
+            bike;
         if(!chainset)
           chainset = $('#chainset_lst').val();
         if(!sprockets)
@@ -183,7 +225,10 @@ Array.prototype.remove = function(from, to) {
         chainset = chainset.split(" ");
         sprockets = sprockets.split(" ");
         wheelsize = wheelsize / 2;
-        bikes.push([chainset, sprockets, wheelsize, cranksize, name, back, fore])
+        bike = [chainset, sprockets, wheelsize, cranksize, name, back, fore];
+        if(gearhub[0].style.display != "none")
+          bike.gearhub = builtin_hubs[gearhub.val()];
+        bikes.push(bike);
         drawGainRatios();
       }
     });
