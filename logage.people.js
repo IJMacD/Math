7a30465423cf823a52967b9b1450e90293ef8bf6
Iain MacDonald <IJMacD@gmail.com>
2013-01-14 23:00:10 +0000

    Implemented log people page

---
+++ logage.people.js
@@ -0,0 +1,22 @@
+$(function(){
+  function logage(birthdate,todate){
+    todate = (typeof todate == "undefined") ? new Date() : todate;
+    return Math.log((todate - birthdate) / (1000 * 60 * 60 * 24 * 365 * 1.0006643835616438)) * Math.LOG10E
+  }
+  var people = $('.people-list li');
+  function draw(pagedelta){
+    var now = new Date();
+    people.each(function(i,item){
+      var item = $(item),
+          birthdate = item.data('birthdate');
+      if(!isFinite(birthdate)){
+        birthdate = Date.parse(birthdate);
+        item.data('birthdate', birthdate);
+      }
+      item.find('p').text(logage(birthdate,now));
+    });
+    requestAnimationFrame(draw);
+  }
+  if(people.length)
+    requestAnimationFrame(draw);
+});
